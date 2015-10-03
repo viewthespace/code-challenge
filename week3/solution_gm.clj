33308bfd316cd17d316303cee03a0f156979159c
Gustavo Matias <gustavo@matias.com>
2015-10-03 07:53:26 -0400

    Now with solution without allocating extra space

--- week3/solution_gm.clj
+++ week3/solution_gm.clj
@@ -1,1 +1,5 @@
+;; allocating extra space
 (clojure.string/join " " (reverse (clojure.string/split "agora eh vida" #" ")))
+
+;; without allocating extra space
+(clojure.string/join (reverse (re-seq #"\w+|\s" "agora eh vida")))
