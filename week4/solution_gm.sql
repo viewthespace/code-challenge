570638ef6a7a2167275ec2dd8d4a2e318598847c
Gustavo Matias <gustavo@matias.com>
2015-10-09 09:48:31 -0400

    solution

---
+++ week4/solution_gm.sql
@@ -0,0 +1,18 @@
+SELECT
+  t.request_at AS day,
+  ROUND(
+    (
+      SELECT
+        COUNT(t1.id)
+      FROM
+        trips AS t1
+      INNER JOIN
+        users AS u1 ON u1.id = t1.client_id AND u1.banned = false AND t1.status LIKE 'cancelled%'
+      WHERE
+        t1.request_at = t.request_at
+    ) / COUNT(t.id)::DECIMAL, 2) AS cancelation_rate
+  FROM
+    trips AS t
+  INNER JOIN
+    users AS u ON u.id = t.client_id
+    AND u.banned = false GROUP BY day;
