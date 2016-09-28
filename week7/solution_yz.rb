ee5efffe6d94d88d5fa2f6a497083c54ca4541ab
Yuriy Zubovski <yuriy.zubovski@viewthespace.com>
2016-09-28 11:26:08 -0400

    adds solution_yz to week7

---
+++ week7/solution_yz.rb
@@ -0,0 +1,13 @@
+# khan_box_common_rows()
+# Runtime: O( N^2 ), N being the height and width of the box
+# Memory: O( N )
+
+def khan_box_common_rows(input)
+  input.map do |row|
+    (row[0] == 1 ? row : row.map { |val| val^1 }).join.to_i(2)
+  end.group_by { |val| val }.values.map(&:length).max
+end
+
+khan_box_common_rows([[0,1,1,0],[1,1,0,0],[1,0,0,1],[1,0,1,1]])
+
+khan_box_common_rows([[1,1,0],[0,1,1],[0,1,0]])
