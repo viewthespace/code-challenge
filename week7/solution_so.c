3bd1193b3f504a5d4a9d7baaaf4dd86864cc3588
shaw3257 <shaw3257@gmail.com>
2016-10-04 11:45:12 -0400

    adds solution_so.c to week7

---
+++ week7/solution_so.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char **argv) {
+  // int n = argv;
+  // int *matrix = malloc(sizeof(int) * (n * n));
+  // for(int i = 0; i < n; i++){
+  //   for(int j = 0; j < n; j++){
+  //     matrix[i * j + j] = 0;
+  //   }
+  // }
+  return 0;
+}
+
+int kahn(int *matrix, int n) {
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+      
+    }
+  }
+  return 0;
+}
+
+
