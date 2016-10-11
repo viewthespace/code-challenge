388e1f3602a4e74b0aa93d16eb334fe948b9953a
Alex Weisberger <alex.weisberger@viewthespace.com>
2016-10-11 08:49:00 -0400

    Can operate on points and not indexes

--- week8/solution_amw.rb
+++ week8/solution_amw.rb
@@ -1,4 +1,5 @@
-def neighbor_coords(row, column, matrix)
+def neighbor_coords(curr_point, matrix)
+  row, column = curr_point
   top = [row - 1, column] if row != 0
   bottom = [row + 1, column] if row != matrix.size - 1
   left =  [row, column - 1] if column != 0
@@ -20,20 +21,6 @@ def min_neighbor_coords(neighbor_coords, matrix)
   end[:coords]
 end
 
-def downstream_min_coords(i, j, min_coords, matrix)
-  neighbor_coords = neighbor_coords(i, j, matrix)
-  min_neighbor_coords = min_neighbor_coords(neighbor_coords, matrix)
-  neighbor_min_coords = neighbor_coords & min_coords
-  if min_coords.include?([i, j])
-    [i, j]
-  elsif neighbor_min_coords.count > 0
-    min_neighbor_coords
-  else
-    new_i, new_j = min_neighbor_coords
-    downstream_min_coords(new_i, new_j, min_coords, matrix)
-  end
-end
-
 # matrix = [
 #   [1, 5, 2],
 #   [2, 4, 7],
@@ -53,32 +40,23 @@ matrix = [
 #
 # TODO:
 # i, j to curr_point
-def go_downstream(i, j, matrix, curr_basin, basins, seen_basins)
-  puts
-  puts "==== Frame ===="
-  puts "(#{i},#{j}) => #{matrix[i][j]}"
+def go_downstream(curr_point, matrix, curr_basin, basins, seen_basins)
+  neighbor_coords = neighbor_coords(curr_point, matrix)
+  min_point = min_neighbor_coords(neighbor_coords, matrix)
 
-  neighbor_coords = neighbor_coords(i, j, matrix)
-  min_i, min_j = min_neighbor_coords(neighbor_coords, matrix)
-
-  if seen_basins[[i, j]]
-    puts "hit cache for (#{i}, #{j})"
-    return seen_basins[[i, j]]
+  if seen_basins[curr_point]
+    return seen_basins[curr_point]
   end
 
-  if [i, j] == [min_i, min_j]
-    puts 'found min'
+  if curr_point == min_point
     basins[$curr_basin] ||= 1
     basin = $curr_basin
-    puts "caching basin: #{basin}"
-    seen_basins[[i, j]] = basin
+    seen_basins[curr_point] = basin
     $curr_basin += 1
     basin
   else
-    puts 'not min, recursing'
-    basin = go_downstream(min_i, min_j, matrix, $curr_basin, basins, seen_basins)
-    puts "caching basin for (#{i},#{j}): #{basin}"
-    seen_basins[[i, j]] = basin
+    basin = go_downstream(min_point, matrix, $curr_basin, basins, seen_basins)
+    seen_basins[curr_point] = basin
     basins[basin] += 1
     basin
   end
@@ -91,35 +69,14 @@ $curr_basin = 0
 seen_basins = {}
 matrix.each_with_index do |row, i|
   row.each_with_index do |element, j|
-    go_downstream(i, j, matrix, $curr_basin, basins, seen_basins)
+    go_downstream([i, j], matrix, $curr_basin, basins, seen_basins)
   end
 end
 
 puts basins.to_s
 puts seen_basins.to_s
 
-# matrix.each_with_index do |row, i|
-#   row.each_with_index do |element, j|
-#     min_coords << [i, j] if element < neighbor_coords(i, j, matrix).map { |coord| element(coord, matrix) }.min
-#   end
-# end
-
-# curr_basin = 0
-# basins = {}
-
-# min_coords.each do |coords|
-#   basins[coords] = curr_basin
-#   curr_basin += 1
-# end
-
-# matrix.each_with_index do |row, i|
-#   row.each_with_index do |element, j|
-#     downstream_min_coords = downstream_min_coords(i, j, min_coords, matrix)
-#     basins[[i, j]] = basins[downstream_min_coords]
-#   end
-# end
-
-# letters = ['A', 'B', 'C', 'D', 'E']
+letters = ['A', 'B', 'C', 'D', 'E']
 
 # matrix.each_with_index do |row, i|
 #   row.each_with_index do |element, j|
