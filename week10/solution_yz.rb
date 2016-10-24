3fbbe96d0e32b255e20c69df59a0a72b869d2f95
Yuriy Zubovski <yuriy.zubovski@viewthespace.com>
2016-10-24 18:07:25 -0400

    adds blazing slow ruby solution_yz because reasons

---
+++ week10/solution_yz.rb
@@ -0,0 +1,51 @@
+# $ week9/profile ruby week10/solution_yz.rb
+# Average run time: 0.58036
+# File size: 1264
+class Week10SolutionYz
+  MAP_TO_PUNCTUATION = /[eqwz]/.freeze
+  FROM_QWERTY = 'bcdfghijklnoprstuvxy'.freeze
+  TO_DVORAK   = 'xjeuidchtnbrlpoygkqf'.freeze
+
+  def initialize(input_file, output_file)
+    @input_file = input_file
+    @output_file = output_file
+    @dvorak_to_qwerty = {}
+  end
+
+  def run
+    prepare_dictionaries
+    find_translations
+  end
+
+  private
+
+  def prepare_dictionaries
+    input = File.open(@input_file)
+    input.each do |word|
+      qwerty = word.downcase.chomp
+      next if qwerty =~ MAP_TO_PUNCTUATION
+      dvorak = qwerty.tr(FROM_QWERTY, TO_DVORAK)
+      @dvorak_to_qwerty[dvorak] = qwerty
+    end
+    input.close
+  end
+
+  def find_translations
+    input = File.open(@input_file)
+    input.each do |word|
+      dvorak = word.downcase.chomp
+      next if dvorak == @dvorak
+      @dvorak = dvorak
+      qwerty = @dvorak_to_qwerty[dvorak]
+      output << "d:#{dvorak}|q:#{qwerty}\n" unless qwerty.nil?
+    end
+    output.close
+    input.close
+  end
+
+  def output
+    @output ||= File.open(@output_file, File::WRONLY | File::APPEND | File::CREAT | File::TRUNC)
+  end
+end
+
+Week10SolutionYz.new('/usr/share/dict/words', 'week10/result_yz.txt').run
