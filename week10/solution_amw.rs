c8102903e4b650ae2af740f25db87bc0570c5d90
Alex Weisberger <alex.weisberger@viewthespace.com>
2016-10-26 22:33:54 -0400

    Send newline separated strings and not vector of strings

--- week10/solution_amw.rs
+++ week10/solution_amw.rs
@@ -22,11 +22,11 @@ fn main() {
     let file = File::open("/usr/share/dict/words").unwrap();
     let lines = BufReader::new(file).lines().map(|line| line.unwrap());
     let words: Vec<String> = lines.filter(|word|
-                             !word.chars().any(|c|
-                                               c == 'q' || c == 'Q' || c == 'w' || c == 'W' ||
-                                               c == 'e' || c == 'E' || c == 'z' || c == 'Z')
+                                          !word.chars().any(|c|
+                                                            c == 'q' || c == 'Q' || c == 'w' || c == 'W' ||
+                                                            c == 'e' || c == 'E' || c == 'z' || c == 'Z')
     ).collect();
-    let iter_size = 5;
+    let iter_size = 7;
     let chunk_size = words.len() / iter_size;
 
     let index: HashSet<String> = words.iter().cloned().collect();
@@ -38,18 +38,16 @@ fn main() {
 
     for i in 0..iter_size {
         let cloned_words = shared_words.clone();
-
-        // Try and Arc this
         let word_index = shared_index.clone();
         let tx_wq = tx_wq.clone();
         thread::spawn(move || {
-            let mut results = vec!();
+            let mut results = String::new();
             for word in cloned_words.iter().skip(i * chunk_size).take(chunk_size) {
                 let converted = word.chars().map(|c|
                                                  QD_MAP[((c.to_ascii_lowercase() as u8) - 97) as usize]
                 ).collect::<String>();
-                if let Some(_) =  word_index.get(&converted) {
-                    results.push(format!("q:{}|d:{}", word, converted));
+                if word_index.contains(&converted) {
+                    results.push_str(format!("q:{}|d:{}\n", word, converted).as_str());
                 }
             }
             tx_wq.send(results).unwrap();
@@ -61,9 +59,8 @@ fn main() {
     while num_chunks_processed < iter_size {
         let producer_results = rx_wq.recv().unwrap();
         num_chunks_processed += 1;
-        results.push_str(producer_results.join("\n").as_str())
+        results.push_str(producer_results.as_str())
     }
-    results.push_str("\n");
     let mut out_file = File::create("words.txt").unwrap();
     let bytes = results.as_bytes();
     out_file.write_all(bytes).unwrap();
