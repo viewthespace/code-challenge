adb71269e75689e4956ffc66219c533eb14df452
Alex Weisberger <alex.weisberger@viewthespace.com>
2016-10-28 17:51:27 -0400

    Nice little Ruby solution .

---
+++ week10/solution_amw.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+require 'set'
+
+q_to_d = {
+  'a' => 'a', 'b' => 'x', 'c' => 'j', 'd' => 'e',
+  'f' => 'u', 'g' => 'i', 'h' => 'd', 'i' => 'c',
+  'j' => 'h', 'k' => 't', 'l' => 'n', 'm' => 'm',
+  'n' => 'b', 'o' => 'r', 'p' => 'l', 'r' => 'p',
+  's' => 'o', 't' => 'y', 'u' => 'g', 'v' => 'k',
+  'x' => 'q', 'y' => 'f'
+}
+
+words = File.read('/usr/share/dict/words').split("\n").map(&:downcase).uniq
+index = Set.new(words)
+words.reject { |word| word =~ /[qwez]/ }.each do |word|
+  converted = word.chars.map { |c| q_to_d[c] }.join.strip
+  puts "q:#{word}|d:#{converted}" if index.member?(converted)
+end
