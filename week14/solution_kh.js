6c8d1ca18e93f062e055ee6c6f655d792e97a598
Kyle Holzinger <kylelholzinger@gmail.com>
2017-03-26 22:30:45 -0400

    [KH] lexicographical sort has failed me

--- week14/solution_kh.js
+++ week14/solution_kh.js
@@ -1,1 +1,13 @@
-(a, b) => a == b?(a < b)*2-1:0
+function (a, b) {
+  let parse = (str) => str.split('.').map(parseInt)
+  let as = parse(a), bs = parse(b)
+
+  let maxList,minlist
+  if(as.length > bs.length){maxlist=as;minlist=bs}
+  for (let i = maxList.length; i > -1; i--) {
+    let x, y
+    if ((x=maxList[i]) != (y=minList[i]))
+      return !!(x > y)
+  }
+  return 0
+}
