e8e31b77fd4f92b56f63c6e4b7af22abc35ef03f
Sam Hone-Studer <swhstuder@gmail.com>
2017-03-28 12:05:04 -0400

    update solution to parse ints

--- week14/solution_sam.rb
+++ week14/solution_sam.rb
@@ -1,3 +1,3 @@
 def comp a, b
-  a.split.zip(b.split).reduce(0) { |s, n| s == 0 ? (n[0] || 0) <=> (n[1] || 0) : s }
+  a.split.zip(b.split).reduce(0) { |s, n| s == 0 ? (n[0].to_i || 0) <=> (n[1].to_i || 0) : s }
 end
