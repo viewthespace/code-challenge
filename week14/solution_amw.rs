c326b3bc9d7dbbc763b2082f20a181f7b72d88f9
Alex Weisberger <alex.weisberger@viewthespace.com>
2017-03-28 08:52:27 -0400

    Rust solution.

---
+++ week14/solution_amw.rs
@@ -0,0 +1,37 @@
+use std::env;
+use std::cmp::Ordering;
+
+fn cmp(v1: &str, v2: &str) -> Option<isize> {
+    match v1.partial_cmp(v2) {
+        Some(Ordering::Less) => Some(-1),
+        Some(Ordering::Greater) => Some(1),
+        _ => None
+    }
+}
+
+fn n(v: &str) -> Vec<&str> {
+    let mut vers_str = v.split(".").collect::<Vec<&str>>();
+    let mut padding = vec!["0", "0", "0"];
+    vers_str.append(&mut padding);
+    vers_str.iter().take(3).cloned().collect()
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let v1 = &args[1];
+    let v2 = &args[2];
+
+    let v1n = n(v1);
+    let v2n = n(v2);
+
+    let compared = v1n.iter()
+        .zip(v2n)
+        .filter_map(|p| cmp(p.0, p.1))
+        .collect::<Vec<isize>>();
+
+    match compared.len() {
+        0 => println!("{}", 0),
+        x if x > 0 => println!("{}", compared[0]),
+        _ => println!("{}", "Not sure what happened.")
+    };
+}
