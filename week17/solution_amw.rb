2db0e13356002c9925f469a4cb44b13dba61e318
Alex Weisberger <alex.weisberger@viewthespace.com>
2017-05-30 13:14:45 -0400

    Rubocop cleanup

--- week17/solution_amw.rb
+++ week17/solution_amw.rb
@@ -1,8 +1,8 @@
 INPUT_SHORT = [
   '172.26.32.162/32',
   '172.26.32.0/24',
-  '172.26.0.0/16',
-]
+  '172.26.0.0/16'
+].freeze
 
 INPUT_FULL = [
   '192.168.0.0/16',
@@ -18,32 +18,40 @@ INPUT_FULL = [
   '172.24.5.1/32',
   '172.24.68.37/32',
   '172.24.168.32/32'
-]
+].freeze
 
-def significant_sections(bits)
+def significant_octets(bits)
   (bits.to_f / 8).ceil
 end
 
 def bit_suffix(cidr)
-  cidr.split("/")[1]
+  cidr.split('/')[1]
+end
+
+def pad_bits(significant_bits)
+  7.downto(8 - significant_bits).reduce(0) { |a, e| a + 2**e }
+end
+
+def max_octets(cidr, significant_octets, num_bits)
+  remaining_bits = num_bits.to_i % 8
+  sections = address_sections(cidr)
+  if remaining_bits.zero?
+    sections.take(significant_octets)
+  else
+    sections.take(significant_octets - 1) << pad_bits(remaining_bits)
+  end
 end
 
 def max_address_sections(cidr)
   num_bits = bit_suffix(cidr)
-  num_sections = significant_sections(num_bits)
-  remaining_bits = num_bits.to_i % 8
-  sections = address_sections(cidr)
-  sections = if remaining_bits == 0
-    sections.take(num_sections)
-  else
-    sections.take(num_sections - 1) << 7.downto(8 - remaining_bits).reduce(0) { |a, i| a + 2 ** i }
-  end
-  (4 - num_sections).times { sections << 255 }
+  num_octets = significant_octets(num_bits)
+  sections = max_octets(cidr, num_octets, num_bits)
+  (4 - num_octets).times { sections << 255 }
   sections
 end
 
 def address_sections(cidr)
-  cidr.split("/")[0].split(".").map(&:to_i)
+  cidr.split('/')[0].split('.').map(&:to_i)
 end
 
 def to_i(sections)
@@ -60,30 +68,35 @@ def to_range(cidr)
   to_i(sections)..to_i(max_sections)
 end
 
+def remove_indexes(collection, indexes)
+  collection.reject.with_index { |_, i| indexes.include? i }
+end
+
+def diagnose_covering_sets(cidr, covering_set)
+  covered = false
+  covering = false
+  deletion_indexes = []
+  new_range = to_range(cidr)
+  covering_set.each_with_index do |covering_cidr, i|
+    if within?(to_range(covering_cidr), new_range)
+      covered = true
+    elsif within?(new_range, to_range(covering_cidr))
+      covering = true
+      deletion_indexes << i
+    end
+  end
+  [covered, covering, deletion_indexes]
+end
+
 def solve(input)
   covering_set = []
-  covering_set << input[0]
-  input.drop(1).each do |cidr|
-    range = to_range(cidr)
-    covered_by = false
-    covering = false
-    indexes = []
-    covering_set.each_with_index do |a, i|
-      if within?(to_range(a), range)
-        covered_by = true
-      elsif within?(range, to_range(a))
-        covering = true
-        indexes << i
-      end
-    end
-    if covering
-      covering_set = covering_set.reject.with_index { |a, i| indexes.include? i }
-    end
-    covering_set << cidr unless covered_by
+  input.each do |cidr|
+    covered, covering, deletion_indexes = diagnose_covering_sets(cidr, covering_set)
+    covering_set = remove_indexes(covering_set, deletion_indexes) if covering
+    covering_set << cidr unless covered
   end
-  puts "Result"
+  puts 'Result'
   puts covering_set
-  puts
 end
 
 solve(INPUT_SHORT)
