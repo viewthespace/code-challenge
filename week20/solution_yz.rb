e982d41f2ab59262532f586ef44a0121d949cd1d
Yuriy Zubovski <yuriy.zubovski@viewthespace.com>
2017-11-15 22:39:50 -0500

    Adds comments to week20/solution_yz.rb for review

--- week20/solution_yz.rb
+++ week20/solution_yz.rb
@@ -1,27 +1,42 @@
 # @param {Integer[][]} nums
 # @return {Integer[]}
 def smallest_range(nums)
+  value_map = value_map_of_list_indices(nums)
   range_size_map_of_ranges = {}
   list_count = nums.size
 
-  value_map = value_map_of_list_indices(nums)
-
+  # for each value, find the first end index that covers every list
   value_map.each_with_index do |(value, list_indices), value_index|
     start_value = end_value = value
 
+    # skip all of the values that precede our starting point
     value_map.drop(value_index + 1).each do |(next_value, next_list_indices)|
+      # if the size of the list_indices hash matches the list count,
+      # then every list is already represented as a key, so we can stop
       break if list_indices.size == list_count
+
+      # if we didn't stop, then the next value becomes the tentative end of the range
       end_value = next_value
+
+      # all of the lists that contain this new end value are now covered by the range
       list_indices.merge!(next_list_indices)
     end
 
+    # if we got here and the list_indices size is smaller than list_count,
+    # then we are so deep in the values that we have passed the end of one or more lists
+    # so we no further valid ranges can be created
     break unless list_indices.size == list_count
+
+    # store the valid range with its size as the key, unless an earlier range of the same size exists
     range_size_map_of_ranges[end_value - start_value] ||= [start_value, end_value]
   end
 
+  # get the range that has the smallest width
   range_size_map_of_ranges[range_size_map_of_ranges.keys.min]
 end
 
+# builds a hash where each key is a index that appears in one or more lists
+# and the value is a hash whose keys are the indices of the lists the outer key came from
 def value_map_of_list_indices(arrays)
   value_map = {}
 
