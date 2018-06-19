e70ac55f743dbbe83d72736109f42ecc61283783
Gino Wu <gino.wu@viewthespace.com>
2018-06-19 14:59:10 -0400

    Update solution_gw.swift

--- week24/solution_gw.swift
+++ week24/solution_gw.swift
@@ -6,56 +6,6 @@
 //  Copyright © 2018 sample. All rights reserved.
 //
 
-/*
- # Songs About Rain
-
- Given a list of integers representing the crossectional elevations of a line,
- calculate the amount of water that would be caught after an infinite amount of
- rain assuming any rain that spills of the side does not build up
-
-
- ## example 1
-
- ### input
- ```javascript
- [ 1, 3, 4, 2, 3, 5, 4, 5 ]
-
- /*
-
- if this were land, this would look like:
-
- x x
- x  xxx
- xx xxxx
- xxxxxxx
- xxxxxxxx
-
- */
- ```
-
- ### output
- ```javascript
- 4
-
-
- /*
-
- explanation:
-
- given this input list, the blocks marked with a '.' would be filled with water
-
- x.x
- x..xxx
- xx.xxxx
- xxxxxxx
- xxxxxxxx
-
- */
- ```
-
-
- */
-
 import XCTest
 
 class SongsAboutRain: XCTestCase {
@@ -68,33 +18,43 @@ class SongsAboutRain: XCTestCase {
         XCTAssertEqual(songAboutRain([1, 2, 3, 2, 1]), 0)
         XCTAssertEqual(songAboutRain([]), 0)
         XCTAssertEqual(songAboutRain([1]), 0)
+        XCTAssertEqual(songAboutRain([5, 2, 2, 1, 2, 4, 2, 1, 3, 5]), 23)
     }
+
 
     func songAboutRain(_ input: [Int]) -> Int {
         var result = 0
         var inBetween: [Int] = []
         var currentPeak: Int?
-        guard input.count > 1 else { return result }
 
         for(index, element) in input.enumerated() {
-            if isPeak(input, currentIndex: index) && currentPeak == nil {
-                currentPeak = element
-            } else if isPeak(input, currentIndex: index) && currentPeak != nil {
-                currentPeak = min(currentPeak!, element)
-                result = inBetween.reduce(result) { $0 + (currentPeak! - $1) }
-                currentPeak = element
-                inBetween.removeAll()
-            } else if !isPeak(input, currentIndex: index) && currentPeak != nil {
+            if isPeak(input, currentIndex: index, currentPeak: currentPeak) {
+                if currentPeak == nil {
+                    currentPeak = element
+                } else {
+                    currentPeak = min(currentPeak!, element)
+                    result = inBetween.reduce(result) { $0 + (currentPeak! - $1) }
+                    currentPeak = element
+                    inBetween.removeAll()
+                }
+            } else if currentPeak != nil {
                 inBetween.append(element)
             }
         }
-
         return result
     }
 
-    func isPeak(_ input: [Int], currentIndex: Int) -> Bool {
-        return currentIndex + 1 == input.count ? input[currentIndex - 1] < input[currentIndex] : input[currentIndex + 1] < input[currentIndex]
+    func isPeak(_ input: [Int], currentIndex: Int, currentPeak: Int?) -> Bool {
+        return currentIndex + 1 == input.count ?
+            isGreaterThanPrevious(currentIndex, input) :
+            isGreaterThanNext(currentIndex, input) && isGreaterThanPrevious(currentIndex, input) && !hasMorePeak(index: currentIndex, input: input, peak: currentPeak)
     }
 
-    
+    func hasMorePeak(index: Int, input: [Int], peak: Int?) -> Bool {
+        guard peak != nil else { return false }
+        return input[index..<input.count].first(where: { $0 > input[index] }) != nil
+    }
+    var isGreaterThanPrevious: ((Int, [Int]) -> Bool) { return { $0 > 0 ? $1[$0 - 1] < $1[$0] : true } }
+    var isGreaterThanNext: ((Int, [Int]) -> Bool) { return { $0 < $1.count ? $1[$0 + 1] < $1[$0] : true } }
+
 }
