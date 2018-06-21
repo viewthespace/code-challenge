24dabee5382759b7a9ced4360427e73978428997
Kyle Holzinger <kylelholzinger@gmail.com>
2018-06-20 23:43:32 -0400

    Now it works

--- week24/solution_kh.js
+++ week24/solution_kh.js
@@ -1,8 +1,8 @@
 let arr = [1, 3, 4, 2, 3, 5, 4, 5]
 
 function volume(xs) {
-  const max = Math.max(xs[0], xs[xs.length - 1])
-  const helper = (xs) => xs.reduce((agg, x) => agg + (max - x), 0)
+  const max = Math.min(xs[0], xs[xs.length - 1])
+  const helper = xs => xs.reduce((agg, x) => agg + (max - x), 0)
 
   return helper(xs.slice(1, -1))
 }
@@ -19,25 +19,16 @@ function WhatsInTheBox(arr) {
     []
   )
 
-  console.log(maxima)
-
   const peaks = maxima.reduce(
     (agg, i, _j, maxima) => (isLocalMinima(i, maxima) ? agg : [...agg, i]),
     []
   )
 
-  console.log(peaks)
-
   let vol = 0
 
   return peaks.slice(1).reduce(
     (agg, x) => ({
-      volume:
-        agg.volume +
-        volume(
-          xs.slice(agg.previous + 1, x),
-          Math.max(xs[x], xs[agg.previous])
-        ),
+      volume: agg.volume + volume(xs.slice(agg.previous, x + 1)),
       previous: x
     }),
     { previous: peaks[0], volume: 0 }
