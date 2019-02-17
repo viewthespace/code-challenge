6ba16a634c20751ef673edcc3162679bf3c11ba0
Alex Weisberger <alex.weisberger@viewthespace.com>
2019-02-16 22:27:08 -0500

    Somehow I've made the solution even better

--- week28--get-in-line/challenge.hs
+++ week28--get-in-line/challenge.hs
@@ -5,58 +5,68 @@ data Slope = Vertical | Horizontal | Sloped Float deriving(Show, Eq)
 data Point = Point Float Float deriving(Eq)
 
 instance Show Point where
-         show (Point x y) = "Point(" ++ show (round x) ++ ", " ++ show (round y) ++ ")"
+  show (Point x y) = "Point(" ++ show (round x) ++ ", " ++ show (round y) ++ ")"
 
-data SlopeReduction = SlopeReduction { point :: Point
-                                     , slope :: Slope
-                                     , pointCount :: Int
-                                     } deriving(Show, Eq)
-
-slopeBetween :: Point -> Point -> Slope
-slopeBetween (Point x1 y1) (Point x2 y2) =
-      if x2 == x1
-         then Vertical
-         else if y2 == y1
-              then Horizontal
-              else Sloped $ (y2 - y1) / (x2 - x1)
-
-makePoint xy = Point (fromIntegral(xy !! 0)) (fromIntegral(xy !! 1))
-toPoints pts = map makePoint pts
+data Line = Line { points :: [Point]
+                 , slope :: Slope
+                 } deriving(Show, Eq)
 
 combinations :: Int -> [a] -> [[a]]
 combinations 0 _ = [[]]
-combinations n xs = [ xs !! i : x | i <- [0..(length xs)-1] 
+combinations n xs = [ xs !! i : x | i <- [0..(length xs)-1]
                                   , x <- combinations (n-1) (drop (i+1) xs) ]
 
-hasSlope :: Slope -> SlopeReduction -> Bool
-hasSlope s r = case r of
-               SlopeReduction { point = _, slope = slope } -> slope == s
+slopeBetween :: Point -> Point -> Slope
+slopeBetween (Point x1 y1) (Point x2 y2)
+  | x2 == x1 = Vertical
+  | y2 == y1 = Horizontal
+  | otherwise =  Sloped $ (y2 - y1) / (x2 - x1)
 
--- reducer :: ([SlopeReduction] -> Point -> Point) -> [SlopeReduction]
-reducer a pts =
-        let pt1 = pts !! 0
-            pt2 = pts !! 1
-            slope = slopeBetween pt1 pt2
-        in case find (hasSlope slope) a of
-             Just SlopeReduction { point = pt, slope = slope, pointCount = count } -> a ++ [SlopeReduction { point = pt, slope = slope, pointCount = count + 1 }]
-             Nothing ->  a ++ [SlopeReduction { point = pt1, slope = slopeBetween pt1 pt2, pointCount = 2 }]
+isPointOnLine :: Slope -> Point -> Line -> Bool
+isPointOnLine s p Line { points = points, slope = slope } =
+  let (pt1:pt2:_) = points
+      (Point x1 y1) = pt1
+      (Point x2 y2) = pt2
+  in slope == s && case slope of
+    Sloped m -> (m * (x2 - x1) - y2 + y1) == 0
+    Vertical -> x1 == x2
+    Horizontal -> y1 == y2
 
-pointCombinations pts = combinations 2 $ toPoints pts 
+lineSegmentIntoLine :: [Line] -> [Point] -> [Line]
+lineSegmentIntoLine a (pt1:pt2:_) =
+  let slope = slopeBetween pt1 pt2
+  in case find (isPointOnLine slope pt1) a of
+    Just Line { points = points, slope = slope }
+      -> a ++ [Line { points = nub $ points ++ [pt1, pt2], slope = slope }]
+    Nothing
+      ->  a ++ [Line { points = [pt1, pt2], slope = slope }]
 
-output r = putStrLn $ show $ r
+pointCombinations :: [(Int, Int)] -> [[Point]]
+pointCombinations pts =
+  combinations 2 $
+    map (\(x, y) -> Point (fromIntegral(x)) (fromIntegral(y))) pts
 
-compareCount r1 r2 = case (r1, r2) of
-             (SlopeReduction { point = _, slope = _, pointCount = count1 }, SlopeReduction { point = _, slope = _, pointCount = count2 }) -> compare count1 count2
+comparePointCount :: Line -> Line -> Ordering
+comparePointCount Line { points = points1 } Line { points = points2 } =
+  compare (length points1) (length points2)
 
--- pts = [[1, 1], [2, 2], [3, 3], [4, 5], [1,2]]
--- pts = [[1,1], [3,3], [4,1], [2,3]]
-pts = [[1,1], [3,2], [5,3], [4,1], [2,3], [1,4]]
-segments = pointCombinations pts
-reductions = foldl reducer [] segments
-main = output $ maximumBy compareCount reductions
+formattedLine :: Line -> [Char]
+formattedLine (Line { points = p, slope = s }) =
+  let (pt1:_) = p
+  in "Point Count: " ++ (show $ length p) ++ " | Slope: "
+    ++ (show s) ++ " | Point: " ++ (show pt1)
 
+lineWithMaxPoints :: [(Int, Int)] -> [Char]
+lineWithMaxPoints pts =
+  let lineSegments = pointCombinations pts
+      lines = foldl lineSegmentIntoLine [] lineSegments
+      maxLine = maximumBy comparePointCount lines
+      in formattedLine maxLine
 
--- pt1 = Point 1 1
--- pt2 = Point 2 2
--- reduction = SlopeReduction { point = pt1, slope = Sloped 1.0, pointCount = 2 }
--- main = output $ (reducer [reduction] pt1 pt2)
+pts1 = [(1,1),(2,2),(3,3)]
+pts2 = [(1,1), (3,2), (5,3), (4,1), (2,3), (1,4)]
+horizontalPts = [(1,1), (2,2), (3,1), (4,1), (5, 1), (2,3), (1,4)]
+verticalPts = [(1,1), (2,2), (3,3), (3,1), (1,2), (1,3), (1,4)]
+pts = [pts1, pts2, horizontalPts, verticalPts]
+
+main = mapM_ print $ map lineWithMaxPoints pts
