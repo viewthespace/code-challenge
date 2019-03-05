ea72cb57620e55a8d8164139460f6cf15b79b2e6
Yuriy Zubovski <yuriy.zubovski@viewthespace.com>
2019-03-04 20:23:24 -0500

    I swear I had this on a single line in my console at first... O(n^2)

--- week028--get-in-line/yuriy.zubovski/solution_yz.rb
+++ week028--get-in-line/yuriy.zubovski/solution_yz.rb
@@ -4,24 +4,44 @@ class GetInLine
   end
 
   def max_line_points
-    raise 'Not Implemented'
+    return points.length if points.length <= 2
+
+    points
+      .uniq
+      .sort_by(&:to_s)
+      .combination(2)
+      .to_a
+      .map(&method(:slope_intercept_first))
+      .uniq
+      .group_by(&:first)
+      .values
+      .map(&:length)
+      .max
+      .+(1)
   end
 
   private
+
+  def slope_intercept_first(segment)
+    x1, y1, x2, y2 = segment.flatten
+    y_diff = y2 - y1
+    x_diff = x2 - x1
+    slope = x_diff == 0 ? nil : y_diff / x_diff.to_f
+    y_intercept = x_diff == 0 ? x1 : y1 - slope * x1 # h4x0r
+    [[slope, y_intercept].to_s, [x1, y1]]
+  end
 
   attr_reader :points
 end
 
 class GetInLineTester
   TESTS = [
-    {
-      input: [[1,1],[2,2],[3,3]],
-      output: 3
-    },
-    {
-      input: [[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]],
-      output: 4
-    }
+    { input: [], output: 0 },
+    { input: [[1,1]], output: 1 },
+    { input: [[1,1],[1,2]], output: 2 },
+    { input: [[1,1],[1,2],[1,3],[2,2]], output: 3 },
+    { input: [[1,1],[2,2],[3,3]], output: 3 },
+    { input: [[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]], output: 4 }
   ]
 
   def initialize(klass = GetInLine)
